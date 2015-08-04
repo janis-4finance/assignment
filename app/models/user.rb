b230d613bde1f095dadda5f3bf2a401da4631f74
janis-4finance <janis-4finance@users.noreply.github.com>
2015-08-04 12:59:47 +0300

    Minor layout improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < Finance::Base
 	validates :phone, presence: true
 	validates :iban,  presence: true, uniqueness: true
 	
-	has_many :loans
+	has_many :loans, lambda{ order( :created_at ) }
 	has_one :outstanding_loan, lambda{ where( 'repaid=0' ).where( 'approved=1' ) }, :class_name => 'Loan'
 	
 end
